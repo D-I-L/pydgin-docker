890aa4eb77891f031abc9183c37b2595de6229db
tcarver <tim.carver@cimr.cam.ac.uk>
2016-03-01 14:39:37 +0000

    add example index load

--- pydgin/entrypoint.sh
+++ pydgin/entrypoint.sh
@@ -6,4 +6,11 @@ python manage.py migrate
 python manage.py makemigrations elastic
 python manage.py migrate elastic
 python manage.py collectstatic --noinput  # Collect static files
+
+# load indices as require
+#sleep 20
+#python manage.py pipeline \
+#    --dir /usr/src/app/src/data-pipeline/data_pipeline/data \
+#    --ini download.ini --sections DISEASE --steps load
+
 /usr/local/bin/gunicorn pydgin.wsgi:application -w 2 -b :8000
